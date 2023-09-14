62ac1528f94eb09cf0b395fa0a685288631d33cc
kyleung1 <leungkyle2@gmail.com>
2023-09-14 00:08:08 -0700

    background, need to rehaul

--- src/routes/Game/+page.svelte
+++ src/routes/Game/+page.svelte
@@ -14,6 +14,7 @@
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    document.body.classList.add("crosshair-cursor");
 
     const enemies: THREE.Mesh[] = [];
 
@@ -28,19 +29,38 @@
       enemies.push(cube);
     }
 
-    // add 10 enemies
-    for (let i = 0; i < 10; i++) {
-      addEnemy();
-    }
+    // target position for mouse
+    let targetx = 0;
+    let targety = 0;
+    let targetz = 15;
 
+    // Function to handle mouse click
     function onMouseClick(event: MouseEvent) {
-      console.log(event);
+      if (selectedModel) {
+        // Dash to the selected model
+        const targetPosition = selectedModel.position.clone();
+        targetx = targetPosition.x;
+        targety = targetPosition.y;
+        targetz = targetPosition.z;
+        console.log("model selected");
+      } else {
+        console.log("no model selected");
+      }
     }
 
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = 15;
-    const spaceTexture = new THREE.TextureLoader().load("space.png");
+    const spaceTexture = new THREE.CubeTextureLoader()
+      .setPath("/textures/spaceMap/")
+      .load([
+        "left.png",
+        "right.png",
+        "top.png",
+        "bottom.png",
+        "front.png",
+        "back.png",
+      ]);
     scene.background = spaceTexture;
 
     // with threejs can only add event listeners to the dom not individual elements, need raycaster
@@ -50,17 +70,18 @@
     const pointer = new THREE.Vector2();
     let selectedModel: THREE.Mesh | null = null;
 
-    function onPointerMove(event: MouseEvent) {
-      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    }
-
     window.addEventListener("resize", () => {
       const newWidth = window.innerWidth;
       const newHeight = window.innerHeight;
       camera.aspect = newWidth / newHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(newWidth, newHeight);
+    });
+
+    const rotationSpeed = 0.01;
+    window.addEventListener("mousemove", (event: MouseEvent) => {
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
     });
 
     window.addEventListener("click", onMouseClick);
@@ -77,11 +98,42 @@
           const material = selectedModel.material as THREE.MeshBasicMaterial;
           material.color.set(0x00ff00);
         }
-        selectedModel = intersects[0].object;
+        selectedModel = intersects[0].object as THREE.Mesh;
+        const material = selectedModel.material as THREE.MeshBasicMaterial;
+        material.color.set(0xff0000);
+      } else {
+        // No intersection, clear the selected model
+        if (selectedModel) {
+          const material = selectedModel.material as THREE.MeshBasicMaterial;
+          material.color.set(0x00ff00);
+        }
+        selectedModel = null;
+      }
+
+      // travels to target when clicked
+      const moveSpeed = 0.2;
+
+      if (
+        camera.position.x !== targetx ||
+        camera.position.y !== targety ||
+        camera.position.z !== targetz
+      ) {
+        const direction = new THREE.Vector3(
+          targetx - camera.position.x,
+          targety - camera.position.y,
+          targetz - camera.position.z,
+        );
+        direction.normalize();
+        camera.position.add(direction.multiplyScalar(moveSpeed));
       }
 
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
+    }
+
+    // add 10 enemies
+    for (let i = 0; i < 10; i++) {
+      addEnemy();
     }
 
     animate();
