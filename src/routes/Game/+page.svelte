d4e5946fc2e13df38eb9abdf9bd5962062cad095
kyleung1 <leungkyle2@gmail.com>
2023-09-16 13:36:22 -0700

    overhaul pointlockcontrols

--- src/routes/Game/+page.svelte
+++ src/routes/Game/+page.svelte
@@ -4,6 +4,7 @@
   import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls.js";
   import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
   import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
+  import { onMount, onDestroy } from "svelte";
 
   if (browser) {
     const scene = new THREE.Scene();
@@ -19,43 +20,29 @@
     document.body.appendChild(renderer.domElement);
     document.body.classList.add("crosshair-cursor");
 
-    class InputController {
-      constructor() {
-        this.initialize();
-      }
-      private current = {
-        leftButton: false,
-        rightButton: false,
-        mouseX: 0,
-        mousey: 0,
-      };
+    let moveForward = false;
+    let moveBackward = false;
+    let moveLeft = false;
+    let moveRight = false;
+    let canJump = false;
 
-      private previous = null;
-      private keys = {};
-      private previousKeys = {};
+    let prevTime = performance.now();
+    const velocity = new THREE.Vector3();
+    const direction = new THREE.Vector3();
+    const vertex = new THREE.Vector3();
+    const color = new THREE.Color();
 
-      initialize() {
-        document.addEventListener(
-          "mousedown",
-          (e) => this.onMouseDown(e),
-          false,
-        );
-        document.addEventListener("mouseup", (e) => this.onMouseUp(e), false);
-        document.addEventListener(
-          "mousemove",
-          (e) => this.onMouseMove(e),
-          false,
-        );
-        document.addEventListener("keydown", (e) => this.onKeyDown(e), false);
-        document.addEventListener("keyup", (e) => this.onKeyUp(e), false);
-      }
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+    let selectedModel: THREE.Mesh | null = null;
 
-      onMouseDown(e: MouseEvent) {}
-      onMouseUp(e: MouseEvent) {}
-      onMouseMove(e: MouseEvent) {}
-      onKeyDown(e: KeyboardEvent) {}
-      onKeyUp(e: KeyboardEvent) {}
-    }
+    // target position for mouse
+    let targetx = 0;
+    let targety = 0;
+    let targetz = 15;
+
+    const controls = new PointerLockControls(camera, document.body);
+    const blocker = document.getElementById("blocker");
 
     const enemies: THREE.Mesh[] = [];
     function addEnemy() {
@@ -68,11 +55,6 @@
       scene.add(cube);
       enemies.push(cube);
     }
-
-    // target position for mouse
-    let targetx = 0;
-    let targety = 0;
-    let targetz = 15;
 
     // Function to handle mouse click
     function onMouseClick(event: MouseEvent) {
@@ -88,27 +70,79 @@
       }
     }
 
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 15;
-    const spaceTexture = new THREE.CubeTextureLoader()
-      .setPath("/textures/spaceMap/")
-      .load([
-        "right.png",
-        "left.png",
-        "top.png",
-        "bottom.png",
-        "front.png",
-        "back.png",
-      ]);
-    scene.background = spaceTexture;
+    function onKeyDown(event: KeyboardEvent) {
+      switch (event.code) {
+        case "ArrowUp":
+        case "KeyW":
+          moveForward = true;
+          break;
+        case "ArrowLeft":
+        case "KeyA":
+          moveLeft = true;
+          break;
 
-    // with threejs can only add event listeners to the dom not individual elements, need raycaster
-    // https://threejs.org/docs/#api/en/core/Raycaster
+        case "ArrowDown":
+        case "KeyS":
+          moveBackward = true;
+          break;
 
-    const raycaster = new THREE.Raycaster();
-    const pointer = new THREE.Vector2();
-    let selectedModel: THREE.Mesh | null = null;
+        case "ArrowRight":
+        case "KeyD":
+          moveRight = true;
+          break;
+        case "Space":
+          if (canJump === true) velocity.y += 350;
+          canJump = false;
+          break;
+      }
+    }
+
+    function onKeyUp(event: KeyboardEvent) {
+      switch (event.code) {
+        case "ArrowUp":
+        case "KeyW":
+          moveForward = false;
+          break;
+
+        case "ArrowLeft":
+        case "KeyA":
+          moveLeft = false;
+          break;
+
+        case "ArrowDown":
+        case "KeyS":
+          moveBackward = false;
+          break;
+
+        case "ArrowRight":
+        case "KeyD":
+          moveRight = false;
+          break;
+      }
+    }
+
+    function addEventBlocker() {
+      blocker?.addEventListener("click", () => {
+        controls.lock();
+      });
+    }
+
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+
+    controls.addEventListener("lock", () => {
+      console.log("it is locked");
+      blocker?.classList.add("hidden");
+    });
+
+    addEventBlocker();
+
+    window.addEventListener("click", onMouseClick);
+
+    controls.addEventListener("unlock", () => {
+      blocker?.classList.remove("hidden");
+      addEventBlocker();
+    });
 
     window.addEventListener("resize", () => {
       const newWidth = window.innerWidth;
@@ -141,17 +175,11 @@
       // controls.lookVertical = false;
     });
 
+    scene.add(controls.getObject());
+
     // const controls = new FirstPersonControls(camera, renderer.domElement);
     // controls.lookSpeed = sensitivity;
     // controls.movementSpeed = 0.2;
-
-    const controls = new FlyControls(camera, document.body);
-    // document.body.addEventListener("click", () => {
-    //   controls.lock();
-    // });
-    // scene.add(controls.getObject());
-
-    window.addEventListener("click", onMouseClick);
 
     function animate() {
       // Update the raycaster
@@ -178,7 +206,7 @@
       }
 
       // update camera rotation based on orientation
-      camera.setRotationFromEuler(cameraOrientation);
+      // camera.setRotationFromEuler(cameraOrientation);
 
       // // travels to target when clicked
       // const moveSpeed = 0.2;
@@ -197,17 +225,148 @@
       //   camera.position.add(direction.multiplyScalar(moveSpeed));
       // }
 
-      controls.update(1);
+      // controls.update(1);
 
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     }
 
-    // add 10 enemies
+    // set the scene
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 15;
+    const spaceTexture = new THREE.CubeTextureLoader()
+      .setPath("/textures/spaceMap/")
+      .load([
+        "right.png",
+        "left.png",
+        "top.png",
+        "bottom.png",
+        "front.png",
+        "back.png",
+      ]);
+    scene.background = spaceTexture;
+
     for (let i = 0; i < 10; i++) {
       addEnemy();
     }
 
     animate();
   }
+
+  // interface current {
+  //   leftButton: boolean;
+  //   rightButton: boolean;
+  //   mouseX: number;
+  //   mouseY: number;
+  // }
+
+  // class InputController {
+  //   constructor() {
+  //     this.initialize();
+  //   }
+  //   public current = {
+  //     leftButton: false,
+  //     rightButton: false,
+  //     mouseX: 0,
+  //     mousey: 0,
+  //   };
+
+  //   public previous: null | current = null;
+  //   public keys: { [keyCode: number]: boolean } = {};
+  //   public previousKeys = {};
+
+  //   initialize() {
+  //     document.addEventListener(
+  //       "mousedown",
+  //       (e) => this.onMouseDown(e),
+  //       false,
+  //     );
+  //     document.addEventListener("mouseup", (e) => this.onMouseUp(e), false);
+  //     document.addEventListener(
+  //       "mousemove",
+  //       (e) => this.onMouseMove(e),
+  //       false,
+  //     );
+  //     document.addEventListener("keydown", (e) => this.onKeyDown(e), false);
+  //     document.addEventListener("keyup", (e) => this.onKeyUp(e), false);
+  //   }
+
+  //   onMouseDown(e: MouseEvent) {
+  //     switch (e.button) {
+  //       case 0: {
+  //         this.current.leftButton = true;
+  //         break;
+  //       }
+  //       case 2: {
+  //         this.current.rightButton = true;
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   onMouseUp(e: MouseEvent) {
+  //     switch (e.button) {
+  //       case 0: {
+  //         this.current.leftButton = false;
+  //         break;
+  //       }
+  //       case 2: {
+  //         this.current.rightButton = false;
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   onMouseMove(e: MouseEvent) {
+  //     this.current.mouseX = e.pageX - window.innerWidth / 2;
+  //     this.current.mousey = e.pageY - window.innerHeight / 2;
+
+  //     if (this.previous === null) {
+  //       this.previous = { ...this.current };
+  //     }
+
+  //     this.current.mouseXDelta = this.current.mouseX - this.previous.mouseX;
+  //     this.current.mouseYDelta = this.current.mouseY - this.previous.mouseY;
+  //   }
+  //   onKeyDown(e: KeyboardEvent) {
+  //     this.keys[e.keyCode] = true;
+  //   }
+  //   onKeyUp(e: KeyboardEvent) {
+  //     this.keys[e.keyCode] = false;
+  //   }
+
+  //   update() {}
+  // }
+
+  // class FirstPersonCamera {
+  //   camera: THREE.PerspectiveCamera;
+  //   input: InputController;
+  //   rotation: THREE.Quaternion;
+  //   translation: THREE.Vector3;
+  //   phi: number;
+  //   theta: number;
+  //   constructor(camera: THREE.PerspectiveCamera) {
+  //     this.camera = camera;
+  //     this.input = new InputController();
+  //     this.rotation = new THREE.Quaternion();
+  //     this.translation = new THREE.Vector3();
+  //     this.phi = 0;
+  //     this.theta = 0;
+  //   }
+
+  //   update(timeElapsedS: number) {
+  //     this.updateRotation(timeElapsedS);
+  //   }
+
+  //   updateRotation(timeElapsedS: number) {
+  //     const xh = this.input.current.mouseX;
+  //   }
+  // }
+
+  // const fpsCamera = new FirstPersonCamera(camera);
 </script>
+
+<!-- {#if showBlocker} -->
+<div class="bg-white w-48 h-48 fixed top-1/2 left-1/2" id="blocker">
+  <h1>Click any where to start</h1>
+</div>
+<!-- {/if} -->
